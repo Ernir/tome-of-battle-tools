864a91a7f8537e0ed1ac722bbb8043cc198a10bf
Ernir <e.ernir@gmail.com>
2020-08-17 23:17:06 +0200

    Add a link to the GraphiQL API to the front page

--- TomeOfBattle/urls.py
+++ TomeOfBattle/urls.py
@@ -7,5 +7,5 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     url(r"^", include("search_engine.urls")),
     path("admin", admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql"),
 ]
