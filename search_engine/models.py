Ernir/tome-of-battle-tools

commit 83a5bb9d0e655c6f8c7ceec175bf3d1e184063c2

Introduce a GraphQL API (#4)

search_engine/models.py
@@ -8,7 +8,9 @@ from search_engine.Managers import ManeuverWithErrataManager, UniqueManeuverMana
 
 class Discipline(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(
+        unique=True, help_text="The URL fragment uniquely identifying the discipline."
+    )
 
     @classmethod  # TODO make this a manager
     def by_count(cls):
@@ -68,7 +70,10 @@ class Action(models.Model):
 
 class Range(models.Model):
     name = models.CharField(max_length=200)
-    is_numeric = models.BooleanField(default=False)
+    is_numeric = models.BooleanField(
+        default=False,
+        help_text="Whether the range can be feasibly interpreted as a number.",
+    )
 
     def __str__(self):
         return self.name
@@ -97,7 +102,10 @@ class Effect(models.Model):
 
 class Duration(models.Model):
     description = models.CharField(max_length=200)
-    is_timed = models.BooleanField(default=False)
+    is_timed = models.BooleanField(
+        default=False,
+        help_text="Whether the duration can be interpreted as a time range.",
+    )
 
     def __str__(self):
         return self.description
@@ -116,7 +124,7 @@ class Maneuver(models.Model):
     discipline = models.ForeignKey(
         Discipline, related_name="maneuvers", on_delete=models.PROTECT
     )
-    type = models.ForeignKey(
+    maneuver_type = models.ForeignKey(
         ManeuverType, related_name="maneuvers", on_delete=models.PROTECT
     )
     descriptor = models.ManyToManyField(Descriptor, blank=True)
@@ -143,7 +151,7 @@ class Maneuver(models.Model):
 
     descriptive_text = models.TextField()  # Markdown-formatted maneuver main text
 
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     html_description = models.TextField()
     page = models.IntegerField()
 
@@ -169,7 +177,7 @@ class Maneuver(models.Model):
             "level": self.level,
             "discipline": self.discipline.name,
             "requirements": self.requirements,
-            "type": self.type.name,
+            "type": self.maneuver_type.name,
             "slug": self.slug,
         }
 

TomeOfBattle/settings.py
@@ -15,6 +15,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="Not secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG_MODE", default=1))
 
+INTERNAL_IPS = ["127.0.0.1"]
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -26,9 +28,14 @@ INSTALLED_APPS = (
     "django.contrib.staticfiles",
     "search_engine",
     "crispy_forms",
+    "graphene_django",
+    "debug_toolbar",
+    "graphiql_debug_toolbar",
 )
 
 CRISPY_TEMPLATE_PACK = "bootstrap3"
+
+GRAPHENE = {"SCHEMA": "schema.schema"}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -38,6 +45,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "graphiql_debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 TEMPLATES = [

TomeOfBattle/urls.py
@@ -1,8 +1,13 @@
+import debug_toolbar
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     url(r"^", include("search_engine.urls")),
     path("admin", admin.site.urls),
+    path("__debug__/", include(debug_toolbar.urls)),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql"),
 ]

dumpschema.sh
@@ -1,0 +1,1 @@
+python manage.py graphql_schema --schema schema.schema --out schema.graphql

schema.graphql
@@ -1,0 +1,101 @@
+schema {
+  query: Query
+}
+
+type ActionType {
+  id: ID!
+  name: String!
+  maneuverSet: [ManeuverType!]!
+}
+
+type AreaType {
+  id: ID!
+  description: String!
+  maneuverSet: [ManeuverType!]!
+}
+
+type DescriptorType {
+  id: ID!
+  name: String!
+  maneuverSet: [ManeuverType!]!
+}
+
+type DisciplineType {
+  id: ID!
+  name: String!
+  slug: String!
+  initiatorClasses: [InitiatorClassType!]!
+  maneuvers: [ManeuverType!]!
+}
+
+type DurationType {
+  id: ID!
+  description: String!
+  isTimed: Boolean!
+  maneuverSet: [ManeuverType!]!
+}
+
+type EffectType {
+  id: ID!
+  description: String!
+  maneuverSet: [ManeuverType!]!
+}
+
+type InitiatorClassType {
+  id: ID!
+  name: String!
+  disciplines: [DisciplineType!]!
+}
+
+type ManeuverType {
+  id: ID!
+  name: String!
+  discipline: DisciplineType!
+  maneuverType: ManeuverTypeType!
+  descriptor: [DescriptorType!]!
+  level: Int!
+  requirements: Int!
+  action: ActionType!
+  range: RangeType!
+  target: TargetType
+  area: AreaType
+  effect: EffectType
+  duration: DurationType
+  savingThrow: SavingThrowType
+  descriptiveText: String!
+  slug: String!
+  htmlDescription: String!
+  page: Int!
+  alternateVersion: ManeuverType
+  hasErrataElsewhere: Boolean!
+  maneuverSet: [ManeuverType!]!
+}
+
+type ManeuverTypeType {
+  id: ID!
+  name: String!
+  maneuvers: [ManeuverType!]!
+}
+
+type Query {
+  allManeuvers: [ManeuverType]
+}
+
+type RangeType {
+  id: ID!
+  name: String!
+  isNumeric: Boolean!
+  maneuverSet: [ManeuverType!]!
+}
+
+type SavingThrowType {
+  id: ID!
+  description: String!
+  maneuverSet: [ManeuverType!]!
+}
+
+type TargetType {
+  id: ID!
+  description: String!
+  maneuverSet: [ManeuverType!]!
+}

schema.py
@@ -1,0 +1,10 @@
+import graphene
+
+import search_engine.schema
+
+
+class Query(search_engine.schema.Query):
+    pass
+
+
+schema = graphene.Schema(query=Query)

search_engine/migrations/0002_auto_20200810_2146.py
@@ -1,0 +1,40 @@
+# Generated by Django 3.0.7 on 2020-08-10 21:46
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("search_engine", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="discipline",
+            name="slug",
+            field=models.SlugField(
+                help_text="The URL fragment uniquely identifying the discipline.",
+                unique=True,
+            ),
+        ),
+        migrations.AlterField(
+            model_name="duration",
+            name="is_timed",
+            field=models.BooleanField(
+                default=False,
+                help_text="Whether the duration can be interpreted as a time range.",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="maneuver", name="slug", field=models.SlugField(unique=True),
+        ),
+        migrations.AlterField(
+            model_name="range",
+            name="is_numeric",
+            field=models.BooleanField(
+                default=False,
+                help_text="Whether the range can be feasibly interpreted as a number.",
+            ),
+        ),
+    ]

search_engine/migrations/0003_auto_20200830_1507.py
@@ -1,0 +1,16 @@
+# Generated by Django 3.0.7 on 2020-08-30 15:07
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("search_engine", "0002_auto_20200810_2146"),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name="maneuver", old_name="type", new_name="maneuver_type",
+        ),
+    ]

commit 1d63767e81b319554434ad61fe35ba332c1c2b51

Reference correct maneuver type field in stat and by-level pages
This fixes a bug introduced in https://github.com/Ernir/tome-of-battle-tools/pull/4

search_engine/models.py
@@ -36,7 +36,7 @@ class ManeuverType(models.Model):
         type_names = [man_type.name for man_type in cls.objects.all()]
         type_overview = {}
         for type_name in type_names:
-            type_num = Maneuver.unique_objects.filter(type__name=type_name).count()
+            type_num = Maneuver.unique_objects.filter(maneuver_type__name=type_name).count()
             type_ratio = int(type_num / Maneuver.unique_objects.count() * 100)
             type_overview[type_name.lower()] = {"num": type_num, "percent": type_ratio}
 

search_engine/templates/maneuvers_by_level.html
@@ -10,7 +10,7 @@
             <ul>
                 {% for maneuver in maneuvers|keyvalue:level|keyvalue:discipline %}
                     <li>
-                        <a href="/maneuvers/{{ maneuver.slug }}/">{{ maneuver.name }}</a> ({{ maneuver.type }})
+                        <a href="/maneuvers/{{ maneuver.slug }}/">{{ maneuver.name }}</a> ({{ maneuver.maneuver_type }})
                     </li>
                 {% endfor %}
             </ul>